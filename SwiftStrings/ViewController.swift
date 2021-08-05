ed4ed0495b971d6bf3ca7aa76189142de3c3d55f
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-08-05 18:31:58 +0300

    Initial Commit

---
+++ SwiftStrings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftStrings
+//
+//  Created by Igor Chernyshov on 05.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
