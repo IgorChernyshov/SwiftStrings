0fd53a302569865e5fd454f00add78cb834807af
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-08-06 18:46:08 +0300

    Completed Day 2

--- SwiftStrings/ViewController.swift
+++ SwiftStrings/ViewController.swift
@@ -9,7 +9,34 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+	@IBOutlet var testLabel: UILabel!
+
+	private var string = "This is a test string"
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		// firstTest()
+		secondTest()
+	}
+
+	private func firstTest() {
+		let attributes: [NSAttributedString.Key: Any] = [
+			.foregroundColor: UIColor.white,
+			.backgroundColor: UIColor.red,
+			.font: UIFont.boldSystemFont(ofSize: 36)
+		]
+
+		let attributedString = NSAttributedString(string: string, attributes: attributes)
+		testLabel.attributedText = attributedString
+	}
+
+	private func secondTest() {
+		let attributedString = NSMutableAttributedString(string: string)
+		attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+		attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+		attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+		attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+		attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+		testLabel.attributedText = attributedString
 	}
 }
